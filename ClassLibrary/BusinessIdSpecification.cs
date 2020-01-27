ed14aa80925884acd5f266132e2cb47062db12db
Minerva <minerva.suvanto@gmail.com>
2020-01-27 13:29:23 +0200

    added tests

--- ClassLibrary/BusinessIdSpecification.cs
+++ ClassLibrary/BusinessIdSpecification.cs
@@ -12,11 +12,10 @@ namespace ClassLibrary
         private IEnumerable<string> _errors;
         public bool IsSatisfiedBy(string entity)
         {
-            //speksit luntattu: https://tarkistusmerkit.teppovuori.fi/tarkmerk.htm#y-tunnus2
+            //speksit: https://tarkistusmerkit.teppovuori.fi/tarkmerk.htm#y-tunnus2
             List<string> errors = new List<string>();
             try
             {
-                //entity = entity.Trim();
                 //annettu arvo pitää olla oikeassa muodossa (NNNNNNN-T)
                 var regex = new Regex(@"^[0-9]{6,7}-[0-9]{1}$");
                 if (string.IsNullOrEmpty(entity) || !regex.IsMatch(entity))
@@ -28,29 +27,29 @@ namespace ClassLibrary
                         entity = entity.Insert(0, "0");
 
                     char[] entityChars = entity.ToCharArray();
-                    int tarkiste = int.Parse(entityChars[entityChars.Length - 1].ToString());
-                    int[] kertoimet = { 7, 9, 10, 5, 8, 4, 2 };
+                    int checkNum = int.Parse(entityChars[entityChars.Length - 1].ToString()); //tarkiste
+                    int[] multipliers = { 7, 9, 10, 5, 8, 4, 2 };
                     int sum = 0;
 
                     //numeroita painotetaan vasemmalta lähtien kertoimilla & tulot lasketaan yhteen
-                    for (int i = 0; i < kertoimet.Length; i++)
+                    for (int i = 0; i < multipliers.Length; i++)
                     {
-                        sum += int.Parse(entityChars[i].ToString()) * kertoimet[i];
+                        sum += int.Parse(entityChars[i].ToString()) * multipliers[i];
                     }
 
                     //laskettu tarkiste
-                    int calcedTarkiste = sum % 11;
+                    int calcedCheckSum = sum % 11;
                     //Jos jakojäännös on 0, tarkistusnumero on 0.
                     //Ei anneta tunnuksia, jotka tuottaisivat jakojäännöksen 1.
-                    if (calcedTarkiste == 1)
+                    if (calcedCheckSum == 1)
                         errors.Add("Tunnuksen tarkiste ei voi olla 1");
                     //Jos jakojäännös on 2..10, tarkistusnumero on 11 miinus jakojäännös. 
-                    else if (calcedTarkiste > 1)
-                        calcedTarkiste = 11 - calcedTarkiste;
+                    else if (calcedCheckSum > 1)
+                        calcedCheckSum = 11 - calcedCheckSum;
 
                     //tarkistetaan täsmääko annettu tarkiste laskettuun tarkisteeseen
-                    if (tarkiste != calcedTarkiste)
-                        errors.Add($"Tunnuksen tarkiste ({tarkiste}) ei täsmää laskettuun tarkisteeseen ({calcedTarkiste})");
+                    if (checkNum != calcedCheckSum)
+                        errors.Add($"Tunnuksen tarkiste ({checkNum}) ei täsmää laskettuun tarkisteeseen ({calcedCheckSum})");
                     else if (errors.Count == 0)
                         return true;
                 }
--- UnitTestProject/BusinessIdSpecificationTest.cs
+++ UnitTestProject/BusinessIdSpecificationTest.cs
@@ -11,20 +11,22 @@ namespace UnitTestProject
         public void TestValidBusinessIds()
         {
             ClassLibrary.ISpecification<string> businessId = new ClassLibrary.BusinessIdSpecification();
-            //ClassLibrary1.BusinessId businessIdClass = new ClassLibrary1.BusinessId();
-            Assert.IsTrue(businessId.IsSatisfiedBy("5700875-9"));
-            Assert.IsTrue(businessId.IsSatisfiedBy("737546-2"));
-            //Assert.IsTrue(businessIdClass.IsSatisfiedBy("15700875-9"));
+            Assert.IsTrue(businessId.IsSatisfiedBy("8517418-0"));
+            Assert.IsTrue(businessId.IsSatisfiedBy("3155130-7"));
+            Assert.IsTrue(businessId.IsSatisfiedBy("3838802-0"));
+            Assert.IsTrue(businessId.IsSatisfiedBy("1566507-2"));
+            Assert.IsTrue(businessId.IsSatisfiedBy("6810385-8"));
         }
 
         [TestMethod]
         public void TestInvalidBusinessIds()
         {
             ClassLibrary.ISpecification<string> businessId = new ClassLibrary.BusinessIdSpecification();
-            //ClassLibrary1.BusinessId businessIdClass = new ClassLibrary1.BusinessId();
-            Assert.IsFalse(businessId.IsSatisfiedBy("a700875-9"));
-            //Assert.IsTrue(businessId.ReasonsForDisatisfaction.Count() > 0);
-            //Assert.IsTrue(businessIdClass.IsSatisfiedBy("15700875-9"));
+            Assert.IsTrue(businessId.IsSatisfiedBy("8a17418-9") == false && businessId.ReasonsForDisatisfaction.Count() > 0);
+            Assert.IsTrue(businessId.IsSatisfiedBy("875-9") == false && businessId.ReasonsForDisatisfaction.Count() > 0);
+            Assert.IsTrue(businessId.IsSatisfiedBy("8700875-1") == false && businessId.ReasonsForDisatisfaction.Count() > 0);
+            Assert.IsTrue(businessId.IsSatisfiedBy("9700875-9") == false && businessId.ReasonsForDisatisfaction.Count() > 0);
+            Assert.IsTrue(businessId.IsSatisfiedBy("18700875-9") == false && businessId.ReasonsForDisatisfaction.Count() > 0);
         }
     }
 }
